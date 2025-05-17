0a7e2ecc424c9e256180682240fc9b529cc17a47
passang25 <outlookdeathless@gmail.com>
2025-05-17 12:48:27 +0530

    add img

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const products = [
     id: 1,
     name: "boAt Airdopes 311 Pro, 50HRS Battery, Fast Charge, Dual Mics ENx Tech, Transparent LID, Low Latency, IPX4, IWP Tech, v5.3 Bluetooth Earbuds, TWS Ear Buds Wireless Earphones with mic (Active Black)",
     description: "High-quality sound with long battery life.",
-    image: "https://via.placeholder.com/150",
+    image: "Boat.jpg",
     link: "https://amzn.to/4msdEun"
   },
   {
